6d79f5fa91386a73a91fc416d7ae7dc298788a84
Cliff L. Biffle <code@cliffle.com>
2024-05-20 16:13:09 -0700

    basics

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ extern crate alloc;
 
 use core::{mem::MaybeUninit, ptr::addr_of};
 
-use alloc::boxed::Box;
+use alloc::vec;
+use device::{gpio::vals::Moder, syscfg::vals::MemMode};
 use panic_halt as _;
-use wartcl::{empty, FlowChange, Env, Value};
+use wartcl::{empty, Env, FlowChange, OwnedValue, Token, Tokenizer, Value};
 use stm32_metapac as device;
 use embedded_alloc::Heap;
 
@@ -19,7 +20,7 @@ static HEAP: Heap = Heap::empty();
 #[cortex_m_rt::entry]
 fn main() -> ! {
     {
-        const HEAP_SIZE: usize = 16384;
+        const HEAP_SIZE: usize = 4096;
         static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
         unsafe {
             HEAP.init(addr_of!(HEAP_MEM) as usize, HEAP_SIZE);
@@ -27,41 +28,167 @@ fn main() -> ! {
     }
 
     let rcc = device::RCC;
-    rcc.ahb4enr().modify(|v| {
+    rcc.apbenr2().write(|w| {
+        w.set_usart1en(true);
+        w.set_syscfgen(true);
+    });
+    rcc.gpioenr().modify(|v| {
         v.set_gpioaen(true);
         v.set_gpioben(true);
         v.set_gpiocen(true);
-        v.set_gpioden(true);
-        v.set_gpioeen(true);
-        // alright, I'm tired of turning on GPIO ports, if we need ports above E
-        // I'll fix it later.
     });
+    device::SYSCFG.cfgr1().write(|w| {
+        w.0 |= (1 << 4) | (1 << 3);
+        w.set_mem_mode(MemMode::MAIN_FLASH);
+    });
+    device::GPIOA.afr(1).write(|w| {
+        w.set_afr(9 - 8, 1); // TX
+        w.set_afr(10 - 8, 1); // RX
+    });
+    device::GPIOA.moder().modify(|w| {
+        w.set_moder(9, Moder::ALTERNATE);
+        w.set_moder(10, Moder::ALTERNATE);
+    });
+    let brr = u16::try_from(16_000_000_u32 / 19_200).unwrap();
+    let uart = device::USART1;
+    uart.brr().write(|w| w.set_brr(brr));
+    uart.cr1().write(|w| {
+        w.set_fifoen(true);
+        w.set_te(true);
+        w.set_re(true);
+        w.set_ue(true);
+    });
+
     cortex_m::asm::isb();
 
-    device::GPIOB.moder().modify(|v| {
-        v.set_moder(0, device::gpio::vals::Moder::OUTPUT);
-    });
-
-    let mut tcl = Env::init();
+    let mut buf = vec![];
+    let mut tcl = Env::default();
     tcl.register(b"setpin", 3, cmd_setpin);
     tcl.register(b"clrpin", 3, cmd_clrpin);
     tcl.register(b"delay", 2, cmd_delay);
-    let _ = tcl.eval(b"\
-            while {subst 1} {\
-                setpin b 0\n\
-                delay 500\n\
-                clrpin b 0\n\
-                delay 500\
-            }\n");
-    panic!("bad");
+    tcl.register(b"puts", 0, cmd_puts);
+    tcl.register(b"heap", 1, cmd_heap);
+    loop {
+        match receive() {
+            8 | 127 => {
+                if buf.pop().is_some() {
+                    emit(8);
+                    emit(b' ');
+                    emit(8);
+                } else {
+                    emit(7);
+                }
+                continue;
+            }
+            c => {
+                buf.push(c);
+                emit(c);
+                if c == b'\r' {
+                    emit(b'\n');
+                }
+                if c != b'\r' && c != b'\n' {
+                    // Don't bother tokenizing until end of line.
+                    continue;
+                }
+            }
+        }
+
+        let mut p = Tokenizer::new(&buf);
+        while let Some(tok) = p.next() {
+            if tok == Token::Error && !p.at_end() {
+                buf.clear();
+                break;
+            }
+
+            if matches!(tok, Token::CmdSep(b'\r' | b'\n')) {
+                let r = tcl.eval(&buf);
+                match r {
+                    Err(_) => {
+                        emit_s(b"ERROR\r\n");
+                    }
+                    Ok(result) => {
+                        emit_s(b"> ");
+                        emit_s(&result);
+                        emit_s(b"\r\n");
+                    }
+                }
+                buf.clear();
+                break;
+            }
+        }
+    }
 }
 
-fn cmd_setpin(_interp: &mut Env, args: &mut [Box<Value>]) -> Result<Box<Value>, FlowChange> {
-    let port = &*args[1];
-    let bank = match port {
+fn emit(byte: u8) {
+    let uart = device::USART1;
+    // Wait for TXE == TXFNF == FIFO not full
+    while !uart.isr().read().txe() {}
+    uart.tdr().write(|w| w.set_dr(u16::from(byte)));
+}
+
+fn emit_s(bytes: &[u8]) {
+    for &b in bytes {
+        emit(b);
+    }
+}
+
+fn receive() -> u8 {
+    let uart = device::USART1;
+    loop {
+        let isr = uart.isr().read();
+        if isr.ore() || isr.fe() || isr.ne() {
+            drain(uart);
+            continue;
+        }
+        if isr.rxne() {
+            return uart.rdr().read().0 as u8
+        }
+    }
+}
+
+fn drain(uart: device::usart::Usart) {
+    while uart.isr().read().rxne() {
+        let _ = uart.rdr().read();
+    }
+    uart.icr().write(|w| {
+        w.set_fe(true);
+        w.set_ne(true);
+        w.set_ore(true);
+    });
+}
+
+fn cmd_puts(_interp: &mut Env, args: &mut [OwnedValue]) -> Result<OwnedValue, FlowChange> {
+    for arg in &args[1..] {
+        emit_s(arg);
+    }
+    emit_s(b"\r\n");
+    Ok(empty())
+}
+
+fn parse_port(port: &Value) -> Result<device::gpio::Gpio, FlowChange> {
+    Ok(match port {
+        b"a" | b"A" => device::GPIOA,
         b"b" | b"B" => device::GPIOB,
+        b"c" | b"C" => device::GPIOC,
+        b"d" | b"D" => device::GPIOD,
         _ => return Err(FlowChange::Error),
-    };
+    })
+}
+
+fn cmd_heap(_interp: &mut Env, _args: &mut [OwnedValue]) -> Result<OwnedValue, FlowChange> {
+    let used = HEAP.used();
+    let free = HEAP.free();
+    let mut text = b"{used ".to_vec();
+    text.extend_from_slice(&wartcl::int_value(used as wartcl::Int));
+    text.extend_from_slice(b" free ");
+    text.extend_from_slice(&wartcl::int_value(free as wartcl::Int));
+    text.push(b'}');
+    Ok(text.into())
+}
+
+fn cmd_setpin(_interp: &mut Env, args: &mut [OwnedValue]) -> Result<OwnedValue, FlowChange> {
+    let port = &*args[1];
+    let bank = parse_port(port)?;
     let pin = wartcl::int(&args[2]);
     if pin < 0 || pin > 15 {
         return Err(FlowChange::Error);
@@ -70,12 +197,9 @@ fn cmd_setpin(_interp: &mut Env, args: &mut [Box<Value>]) -> Result<Box<Value>, 
     Ok(empty())
 }
 
-fn cmd_clrpin(_interp: &mut Env, args: &mut [Box<Value>]) -> Result<Box<Value>, FlowChange> {
+fn cmd_clrpin(_interp: &mut Env, args: &mut [OwnedValue]) -> Result<OwnedValue, FlowChange> {
     let port = &*args[1];
-    let bank = match port {
-        b"b" | b"B" => device::GPIOB,
-        _ => return Err(FlowChange::Error),
-    };
+    let bank = parse_port(port)?;
     let pin = wartcl::int(&args[2]);
     if pin < 0 || pin > 15 {
         return Err(FlowChange::Error);
@@ -84,7 +208,7 @@ fn cmd_clrpin(_interp: &mut Env, args: &mut [Box<Value>]) -> Result<Box<Value>, 
     Ok(empty())
 }
 
-fn cmd_delay(_interp: &mut Env, args: &mut [Box<Value>]) -> Result<Box<Value>, FlowChange> {
+fn cmd_delay(_interp: &mut Env, args: &mut [OwnedValue]) -> Result<OwnedValue, FlowChange> {
     let interval = wartcl::int(&args[1]);
     // Assuming our clock frequency is 64 MHz.
     cortex_m::asm::delay(interval as u32 * 64_000);
